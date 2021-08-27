acab6c4bf396cecde93fe946d2b9c7fada75868e
Alex Rosario <alex.rosarioh@outlook.com>
2021-08-26 20:31:09 -0400

    Start C# version of tic-tac-toe game.

---
+++ ttt-cs/GameConstants.cs
@@ -0,0 +1,9 @@
+namespace TicTacToe {
+  class GameConstants {
+    static readonly int ROWS = 3;
+    static readonly int COLS = 3;
+    static readonly int O = 0;
+    static readonly int X = 1;
+    static readonly int EMPTY = -1;
+  }
+}
---
+++ ttt-cs/PlayGame.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace TicTacToe {
+  class PlayGame {
+    static void Main(string[] args) {
+      GameData tic = new GameData();
+      bool playAgain = false;
+      
+      do {
+        Console.WriteLine("Would you like to play again?");
+        Console.WriteLine("(1) Yes");
+        Console.WriteLine("(0) No");
+        
+        do {
+          bool invalidChoice = false;
+          char choice = Console.ReadKey().KeyChar;
+          switch(choice) {
+            case '1':
+              playAgain = true;
+              break;
+            case '0':
+              break;
+            default:
+              Console.WriteLine("Invalid input.");
+          }
+        } while(invalidChoice);
+        
+      } while(playAgain);
+    }
+  }
+}
